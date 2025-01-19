b6ac70396fec2c7eef5f0bb080814031503f7125
MateuszCzerniawski <128147019+MateuszCzerniawski@users.noreply.github.com>
2025-01-19 16:17:19 +0100

    basic trees visualization done

--- scripts/Visualizer.py
+++ scripts/Visualizer.py
@@ -1,13 +1,23 @@
 import math
 import os
 import re
+import warnings
 
 import cv2
 import numpy as np
+import pandas as pd
 from matplotlib import pyplot as plt
 
 from scripts import DataManipulator, Util
 
+warnings.filterwarnings("ignore")
+
+colors = ['blue', 'red', 'green', 'orange', 'purple', 'lime', 'black']
+missing_dt_values = [('max_features', 'None'), ('min_samples_split', 2), ('max_depth', 'None'),
+                     ('criterion', '\'gini\'')]
+missing_et_values = [('n_estimators', 100), ('max_features', 'sqrt'), ('min_samples_split', 2), ('max_depth', 'None'),
+                     ('criterion', '\'gini\'')]
+missing_rf_values=missing_et_values
 
 def visualise_symbols(parsed_images_dir, symbols_dir, output_path):
     blended = dict()
@@ -67,20 +77,61 @@ def visualise_pca_variance(input_dir, output_path):
             all[name[:-2]] = (dims, vars)
     plt.close()
     fig, axes = plt.subplots(3, 1, figsize=(10, 10))
-    colors = ['blue', 'red', 'green', 'orange', 'purple', 'lime', 'black']
     axes[0].set_title('original')
     axes[1].set_title('bilinear')
     axes[2].set_title('nearest')
-    counters=[0,0,0]
+    counters = [0, 0, 0]
     for name in all.keys():
         tmp = all[name]
         index = 1 if 'bilinear' in name else (2 if 'nearest' in name else 0)
-        color=colors[counters[index]] if counters[index]<len(colors) else 'blue'
-        axes[index].plot(tmp[0], tmp[1], label=name,color=color)
-        counters[index]+=1
+        color = colors[counters[index]] if counters[index] < len(colors) else 'blue'
+        axes[index].plot(tmp[0], tmp[1], label=name, color=color)
+        counters[index] += 1
         axes[index].set_label(name)
         print(name)
     axes[0].legend()
     axes[1].legend()
     axes[2].legend()
     Util.save_plot(output_path)
+
+
+def visualise_results(input_path, output_path, missing=None):
+    data = pd.read_csv(input_path)
+    param_dict = dict()
+    for index, row in data.iterrows():
+        params = re.search(r'\(([^()]*|\([^()]*\))*\)', row['model']).group(0)[1:-1]
+        params = params.replace(' ', '')
+        if missing is not None:
+            for m in missing:
+                if m[0] not in params:
+                    params = f'{m[0]}={m[1]},' + params
+        params = [i.replace('\n', '') for i in params.split(',')]
+        for p in params:
+            param, val = p.split('=')
+            if param not in param_dict:
+                param_dict[param] = dict()
+            elif val not in param_dict[param]:
+                param_dict[param][val] = []
+            else:
+                param_dict[param][val].append((row['accuracy'], row['time']))
+    plt.close()
+    fig, axes = plt.subplots(1, len(param_dict.keys()), figsize=(5 * len(param_dict.keys()), 5))
+    index = -1
+    for param in param_dict:
+        index += 1
+        tmp = 0
+        axes[index].set_title(str(param))
+        for val in param_dict[param]:
+            accuracies = [i[0] for i in param_dict[param][val]]
+            accuracies = pd.value_counts(pd.cut(accuracies, bins=10), sort=False)
+            color = colors[tmp] if tmp < len(colors) else 'black'
+            accuracies.plot(kind='bar', ax=axes[index], position=tmp, width=0.15, color=color, label=str(val))
+            tmp += 1
+        axes[index].legend()
+    plt.tight_layout()
+    Util.save_plot(output_path)
+
+
+visualise_results('../results/DecisionTree_res', '../graphs/DTacc', missing=missing_dt_values)
+visualise_results('../results/ExtraTrees_res', '../graphs/ETacc', missing=missing_et_values)
+visualise_results('../results/RandomForest_res', '../graphs/RFacc',missing=missing_rf_values)
